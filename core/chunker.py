a924a5b471867b89edae4f0b1c640afafcf7d05a
yeti-s <sel9371@gmail.com>
2024-11-26 12:38:49 +0900

    fix: change default chunk size

--- core/chunker.py
+++ core/chunker.py
@@ -14,11 +14,11 @@ class Chunker(ABC):
 
 class RecursiveChunker(Chunker):
     def chunk(self, text: str) -> list[str]:
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=56)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=56)
         docs = text_splitter.split_text([text])
         return [doc.page_content for doc in docs]
     
     def split_documents(self, documents: list[str]) -> list[str]:
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=56)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=56)
         docs = text_splitter.split_documents(documents)
         return [doc.page_content for doc in docs]

74451b1e21a6b6ace9df753b724f6d116acb5490
yeti-s <sel9371@gmail.com>
2024-11-26 20:52:19 +0900

    fix: change default embedding module

--- web_search/nodes/semantic_search_node.py
+++ web_search/nodes/semantic_search_node.py
@@ -1,6 +1,6 @@
+from FlagEmbedding import BGEM3FlagModel
 from langchain_core.documents import Document
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from sklearn.metrics.pairwise import cosine_similarity
 
 from core.chunker import Chunker
@@ -17,9 +17,9 @@ def chunk(chunker:Chunker, pages:list[SearchResult]) -> list[Document]:
     docs = WebBaseLoader(links).load_and_split(chunker)
     return docs
 
-def semantic_search(model:HuggingFaceBgeEmbeddings, query:str, chunks:list[Document]):
-    q_embedding = model.embed_query(query)  # Embed the query
-    embeddings = model.embed_documents(chunks)  # Embed the documents
-    similarities = cosine_similarity([q_embedding], embeddings).flatten()  # Compute cosine similarity
+def semantic_search(model:BGEM3FlagModel, query:str, chunks:list[Document]):
+    q_embedding = embed(model, query)
+    embeddings = embed(model, chunks)
+    similarities = cosine_similarity(q_embedding, embeddings).flatten()  # Compute cosine similarity
     sorted_indices = similarities.argsort()[::-1]  # Sort by similarity in descending order
     return [chunks[i] for i in sorted_indices[:15]]  # Return sorted chunks

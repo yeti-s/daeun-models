11ec0b27bcfb21cb240ee0b5d4e038fd0c149953
yeti-s <sel9371@gmail.com>
2024-11-26 15:14:25 +0900

    feat: create query with current date

--- web_search/nodes/to_query_node.py
+++ web_search/nodes/to_query_node.py
@@ -1,13 +1,21 @@
+from datetime import datetime
+
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, BaseMessage
+
 from core.generator import generate
 
-system_prompt = "Please write a query for web searching to answer the question."
-user_prompt = "Question:\n{query}\Query:\n"
+SYSTEM_PROMPT = f"""You are an AI assistant that generates web search query based on the user's questions.
+If necessary, refer to previous conversations to generate the search query.
+Write the search query only without anything.
 
-def to_query(llm:ChatOpenAI, query:str) -> str:
-    message = generate(llm, [
-        SystemMessage(content=system_prompt),
-        HumanMessage(content=user_prompt.format(query=query))
-    ])
-    return message.content
+Additional Information:
+Current date: {datetime.now().strftime("%a, %d %B, %Y, %I:%M%p")}"""
+
+USER_PROMPT = "Question:\n{query}\Query:\n"
+
+def to_query(llm:ChatOpenAI, query:str, history:list[BaseMessage]) -> str:
+    messages = [SystemMessage(content=SYSTEM_PROMPT)]
+    messages.extend(history)
+    messages.append(HumanMessage(content=USER_PROMPT.format(query=query)))
+    return generate(llm, messages).content

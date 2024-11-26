16347b85f2e02aee5e74cbcdc1ad8f6e4513af6a
yeti-s <sel9371@gmail.com>
2024-11-26 20:53:49 +0900

    fix: log with utf-8 encoding

--- core/utils.py
+++ core/utils.py
@@ -8,5 +8,6 @@ def init_logging(level=logging.INFO, file=None):
         filename=os.path.join('logs', f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log') if file is None else file,
         format = '%(asctime)s [%(levelname)s] %(message)s',
         datefmt = '%Y-%m-%d %I:%M:%S %p',
-        level=level
+        level=level,
+        encoding='utf-8'
     )    

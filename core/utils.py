617538844fbeecb86df40565191e7fd1b40b1342
yeti-s <sel9371@gmail.com>
2024-11-26 15:15:03 +0900

    fix: change log name

--- core/utils.py
+++ core/utils.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def init_logging(level=logging.INFO, file=None):
     os.makedirs('logs', exist_ok=True)
     logging.basicConfig(
-        filename=os.path.join('logs', f'{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}.log') if file is None else file,
+        filename=os.path.join('logs', f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log') if file is None else file,
         format = '%(asctime)s [%(levelname)s] %(message)s',
         datefmt = '%Y-%m-%d %I:%M:%S %p',
         level=level

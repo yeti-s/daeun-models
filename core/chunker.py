f9be13218f1eb15153e638250c960c48fe40f53c
yeti-s <sel9371@gmail.com>
2024-11-26 11:22:51 +0900

    fix: add split documents method

--- core/chunker.py
+++ core/chunker.py
@@ -5,11 +5,20 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class Chunker(ABC):
     @abstractmethod
-    def chunk(cls, text: str) -> list[str]:
+    def chunk(self, text: str) -> list[str]:
+        pass
+    
+    @abstractmethod
+    def split_documents(self, documents: list[str]) -> list[str]:
         pass
 
 class RecursiveChunker(Chunker):
     def chunk(self, text: str) -> list[str]:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=56)
-        docs = text_splitter.create_documents([text])
+        docs = text_splitter.split_text([text])
+        return [doc.page_content for doc in docs]
+    
+    def split_documents(self, documents: list[str]) -> list[str]:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=56)
+        docs = text_splitter.split_documents(documents)
         return [doc.page_content for doc in docs]

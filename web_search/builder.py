b763b59e0bf4b73950a88303e76f64bb7fb7d85c
yeti-s <sel9371@gmail.com>
2024-11-26 15:16:23 +0900

    feat: add langgraph for answering based on web search

---
+++ web_search/builder.py
@@ -0,0 +1,123 @@
+import time
+import logging
+
+from typing_extensions import TypedDict
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage
+from langchain_core.documents import Document
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langgraph.graph import StateGraph, START, END
+
+from core.chunker import Chunker
+from core.search import SearchEngine
+from .nodes import (
+    to_query,
+    search, chunk, semantic_search,
+    generate_answer
+)
+
+INITIALIZE = "INITIALIZE_NODE"
+TO_QUERY = "TO_QUERY_NODE"
+SEMANTIC_SEARCH = "SEMANTIC_SEARCH_NODE"
+GENERATE_ANSWER = "GENERATE_ANSWER_NODE"
+
+# ==================================================================================
+# =============================== define states ====================================
+# ==================================================================================
+
+
+class State(TypedDict):
+    node:str
+    history:list[BaseMessage]
+    elapsed:float
+    # inputs
+    generator:ChatOpenAI
+    engine:SearchEngine
+    chunker:Chunker
+    embedder:HuggingFaceBgeEmbeddings
+    question:str
+    # created
+    query:str
+    passages:list[Document]
+    answer_stream:str
+    
+    
+# ==================================================================================
+# =============================== define nodes =====================================
+# ==================================================================================
+
+def initialize_node(state: State) -> dict:
+    return {
+        'node': INITIALIZE,
+        'elapsed': time.time()
+    }
+
+def to_query_node(state: State) -> dict:
+    history = state['history']
+    generator = state['generator']
+    question = state['question']
+    query = to_query(generator, question, history)
+    logging.debug(f"[Elapsed:{TO_QUERY}] {time.time() - state['elapsed']:.2f}")
+
+    return {
+        'node': TO_QUERY,
+        'query': query
+    }
+    
+def semantic_search_node(state: State) -> dict:
+    engine = state['engine']
+    embedder = state['embedder']
+    chunker = state['chunker']
+    query = state['query']
+    pages = search(engine, query)
+    chunks = chunk(chunker, pages)
+    passages = semantic_search(embedder, query, chunks)
+    logging.debug(f"[Elapsed:{SEMANTIC_SEARCH}] {time.time() - state['elapsed']:.2f}")
+    
+    return {
+        'node': SEMANTIC_SEARCH,
+        'passages': passages
+    }
+
+def generate_answer_node(state: State) -> dict:
+    generator = state['generator']
+    question = state['question']
+    passages = state['passages']
+    answer_stream = generate_answer(generator, question, passages)
+    logging.debug(f"[Elapsed:{GENERATE_ANSWER}] {time.time() - state['elapsed']:.2f}")
+    
+    return {
+        'node': GENERATE_ANSWER,
+        'answer_stream': answer_stream,
+        'elapsed': time.time() - state['elapsed']
+    }
+
+
+# ==================================================================================
+# ============================= define routeres ====================================
+# ==================================================================================
+
+
+
+
+# ==================================================================================
+# =============================== define graph =====================================
+# ==================================================================================
+
+graph_builder = StateGraph(State)
+
+# create nodes
+graph_builder.add_node(INITIALIZE, initialize_node)
+graph_builder.add_node(TO_QUERY, to_query_node)
+graph_builder.add_node(SEMANTIC_SEARCH, semantic_search_node)
+graph_builder.add_node(GENERATE_ANSWER, generate_answer_node)
+
+# create edges
+graph_builder.add_edge(START, INITIALIZE)
+graph_builder.add_edge(INITIALIZE, TO_QUERY)
+graph_builder.add_edge(TO_QUERY, SEMANTIC_SEARCH)
+graph_builder.add_edge(SEMANTIC_SEARCH, GENERATE_ANSWER)
+graph_builder.add_edge(GENERATE_ANSWER, END)
+
+# compile graph
+graph = graph_builder.compile()

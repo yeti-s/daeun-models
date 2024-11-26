ebc6ce8f9c06a9714c36039ea690f5e3d712b83f
yeti-s <sel9371@gmail.com>
2024-11-26 15:14:32 +0900

    fix: change prompt

--- web_search/nodes/generate_answer_node.py
+++ web_search/nodes/generate_answer_node.py
@@ -3,9 +3,17 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, BaseMessageChunk
 from core.generator import stream
 
-SYSTEM_PROMPT = """You are an AI assistant to answer question using the provided contexts in Korean.
+SYSTEM_PROMPT = """You are a friendly and helpful AI assistant. Find relevant information from the context to answer the user's questions, and generate responses in a natural and friendly tone as if conversing with a person.
+Detailed instructions:
+- Always maintain a polite and kind attitude.
+- Carefully analyze the user's question and find relevant information from the context.
+- Write responses that are concise yet informative.
+- Explain technical terms or complex concepts in an easy-to-understand manner.
+- If necessary, ask additional questions to clarify the user's intent.
+- Do not use emoji.
+- Answer must be written in Korean.
 
-Contexts:
+Context:
 {passages}
 """
 

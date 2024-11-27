fe91d776004ae81b6131a1235c4fdbd378f1fa79
yeti-s <sel9371@gmail.com>
2024-11-27 19:14:59 +0900

    feat: limit num chunks

--- web_search/nodes/semantic_search_node.py
+++ web_search/nodes/semantic_search_node.py
@@ -17,9 +17,9 @@ def chunk(chunker:Chunker, pages:list[SearchResult]) -> list[Document]:
     docs = WebBaseLoader(links).load_and_split(chunker)
     return docs
 
-def semantic_search(model:BGEM3FlagModel, query:str, chunks:list[Document]):
+def semantic_search(model:BGEM3FlagModel, query:str, chunks:list[Document], top_k:int=10):
     q_embedding = embed(model, query)
     embeddings = embed(model, chunks)
     similarities = cosine_similarity(q_embedding, embeddings).flatten()  # Compute cosine similarity
     sorted_indices = similarities.argsort()[::-1]  # Sort by similarity in descending order
-    return [chunks[i] for i in sorted_indices[:15]]  # Return sorted chunks
+    return [chunks[i] for i in sorted_indices[:top_k]]  # Return sorted chunks

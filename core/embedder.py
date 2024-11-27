095d1c472a11584fa0aa5b6a91807cc7024fa7ae
yeti-s <sel9371@gmail.com>
2024-11-27 19:43:59 +0900

    fix: set default chunk size

--- core/embedder.py
+++ core/embedder.py
@@ -4,7 +4,7 @@ from FlagEmbedding import BGEM3FlagModel
 # example of init model
 # model = BGEM3FlagModel(EMBEDDING_LLM_NAME,  use_fp16=True)
 
-def embed(model:BGEM3FlagModel, text: Union[str, list[str]], batch_size=8, max_length=512) -> list[list[float]]:
+def embed(model:BGEM3FlagModel, text: Union[str, list[str]], batch_size=8, max_length=256) -> list[list[float]]:
     text = [text] if type(text) == str else text
     return model.encode(
         text,
